45e18f8170296dca1268523f843578927c765102
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-05-29 10:33:03 +0200

    homeWork7InputSelectEnd

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -35,7 +35,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         if (onChangeOption) {
-            return onChangeOption(e.currentTarget.value)
+            onChangeOption(+e.currentTarget.value)
         }
 
     }
