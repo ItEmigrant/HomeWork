0c54f38c4d22f9e872106fc1f6826364baf6244f
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-06-06 12:24:57 +0200

    homeWork10start

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,8 +1,12 @@
+type initStateType = {
+    isLoading: boolean
+}
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
 
